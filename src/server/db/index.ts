b0ccdea98a80dcbe11f359fe926c99d7d1d5d016
Ujen Basi <ujenbasi12@gmail.com>
2024-02-01 20:01:27 +0545

    changed database connecting code

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,23 +1,7 @@
-import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+
 import { env } from "~/env.mjs";
 import * as schema from "./schema";
-export * from "./schema";
 
-declare global {
-  // eslint-disable-next-line no-var -- only var works here
-  var db: PostgresJsDatabase<typeof schema> | undefined;
-}
-
-let db: PostgresJsDatabase<typeof schema>;
-
-if (env.NODE_ENV === "production") {
-  db = drizzle(postgres(env.DATABASE_URL), { schema });
-} else {
-  if (!global.db) {
-    global.db = drizzle(postgres(env.DATABASE_URL), { schema });
-  }
-  db = global.db;
-}
-
-export { db };
+export const db = drizzle(postgres(env.DATABASE_URL), { schema });
