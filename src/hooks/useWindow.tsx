5b8a9d533ee6fad32bc93777771432b1b8aa65c7
Ujen Basi <ujenbasi12@gmail.com>
2024-02-01 19:24:29 +0545

    fix: fixed usewindow bug

--- src/hooks/useWindow.tsx
+++ src/hooks/useWindow.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 
 const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 0,
+    height: 0,
   });
 
   useEffect(() => {
